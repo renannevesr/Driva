200406888a53a336a9b4bf0a83dabe1c5b5ab340
Renan Neves <renan.neves@clicksoft.com.br>
2023-08-29 00:32:16 -0300

    feat: saving data

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 browser = webdriver.Chrome()
-palavras = 'estagio'
+job_data = []
+words = 'estagio'
+
 browser.get("https://portal.gupy.io")
 time.sleep(1)
 
-browser.find_element(By.XPATH,'//*[@id="react-aria-3"]').send_keys(palavras)
+browser.find_element(By.XPATH,'//*[@id="react-aria-3"]').send_keys(words)
 browser.find_element(By.XPATH, '//*[@id="main-content"]/div/div/div[2]/div/div/div/div/div[2]/button').click()
 time.sleep(2)
 html_content = browser.page_source
@@ -25,7 +27,7 @@ for element in strong_elements:
     match = pattern.search(text)
     if match:
         integer_value = int(match.group(1))
-        scrolls = integer_value // 9
+        scrolls = integer_value // 2000
 
         for _ in range(scrolls):
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -37,7 +39,8 @@ for element in elements_to_remove:
     matches = soup.find_all(element)
     for match in matches:
         match.decompose()
-        
+  
+           
 jobs = soup.find("ul", class_="zVzmE")
 jobs_boxes = jobs.find_all('li')
 
@@ -45,11 +48,17 @@ for job in jobs:
     company = job.find("p", class_="cQyvth").text
     title = job.find("h2", class_="XNNQK").text
     span_elements = job.find_all("span", class_="cezNaf")
-    print(span_elements)
     location = span_elements[0].text if len(span_elements) > 0 else ""
     work_modality = span_elements[1].text if len(span_elements) > 1 else ""
     job_type = span_elements[2].text if len(span_elements) > 2 else ""
     pcd_status = span_elements[2].text if len(span_elements) > 2 else ""
     date = job.find("p", class_="inqtnx").text
+    job_data.append([company, title, location, work_modality, job_type, pcd_status, date])
     
     
+with open('jobs_data.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Company', 'Title', 'Location', 'Work Modality', 'Job Type', 'PCD Status', 'Date'])
+    writer.writerows(job_data)
+
+
