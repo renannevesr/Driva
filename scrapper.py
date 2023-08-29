06ed24e49a4c5ebb26f12458ded710042469017e
Renan Neves <renan.neves@clicksoft.com.br>
2023-08-29 01:48:21 -0300

    docs: scrapper

--- scrapper.py
+++ scrapper.py
@@ -7,6 +7,7 @@ from time import sleep
 
 browser = webdriver.Chrome()
 job_data = []
+#escolhi 4 palavras para realizar a busca no site.
 words = ['estagio', 'jr', 'pleno', 'senior']
 for word in words:
     browser.get("https://portal.gupy.io")
@@ -20,15 +21,16 @@ for word in words:
 
     # Definindo um padrão de expressão regular para encontrar o número de vagas
     pattern = re.compile(r'(\d+)\s+vagas')
-
+    # Analisei que a gupy retorna para o site a quantidade total de vagas e ela renderiza em blocos
     strong_elements = soup.find_all('strong')
     for element in strong_elements:
         text = element.get_text()
         match = pattern.search(text)
         if match:
             integer_value = int(match.group(1))
-            scrolls = integer_value // 2000
-            # Realizando o scroll da página para carregar mais vagas
+            # A gupy renderiza a parte de vagas por blocos de 9
+            scrolls = integer_value // 9
+            # Realizando o scroll da página para carregar mais blocos de 9
             for _ in range(scrolls):
                 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(1)  
@@ -41,17 +43,22 @@ for word in words:
     
     # Encontrando as informações dos trabalhos
     jobs = soup.find("ul", class_="zVzmE")
+    # A gupo cria lista unica para cada vaga
     jobs_boxes = jobs.find_all('li')
 
     for job in jobs:
+        #Apenas encontrei o padrão que o site foi construido para pegar todas as informações
         company = job.find("p", class_="cQyvth").text
         title = job.find("h2", class_="XNNQK").text
+        date = job.find("p", class_="inqtnx").text
+        #Todos esses campos abaixo estão com o mesmo nome da classe 
         span_elements = job.find_all("span", class_="cezNaf")
+        # Criei uma logica para percorer os span na lista
         location = span_elements[0].text if len(span_elements) > 0 else ""
         work_modality = span_elements[1].text if len(span_elements) > 1 else ""
         job_type = span_elements[2].text if len(span_elements) > 2 else ""
         pcd_status = span_elements[3].text if len(span_elements) > 3 else ""
-        date = job.find("p", class_="inqtnx").text
+        
         # Adicionando informações do trabalho à lista de dados
         job_data.append([company, title, location, work_modality, job_type, pcd_status, date, word])
         
