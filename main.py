c66e15c1f4edb43f45b92f931e7de713cb4541e3
Renan Neves <renan.neves@clicksoft.com.br>
2023-08-28 23:51:37 -0300

    feat: home selenium

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import requests, time, csv
+import requests, time, csv, re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
 
-driver = webdriver.Chrome()
+browser = webdriver.Chrome()
+palavras = 'estagio'
+browser.get("https://portal.gupy.io")
+time.sleep(2)
 
-driver.get("https://portal.gupy.io")
-time.sleep(20)
+browser.find_element(By.XPATH,'//*[@id="react-aria-3"]').send_keys(palavras)
+browser.find_element(By.XPATH, '//*[@id="main-content"]/div/div/div[2]/div/div/div/div/div[2]/button').click()
+time.sleep(2)
+html_content = browser.page_source
+soup = BeautifulSoup(html_content, 'html.parser')
